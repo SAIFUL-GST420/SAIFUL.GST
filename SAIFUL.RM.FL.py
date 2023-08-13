d2dfc85f0b0b608da6b7288a5d975bd69d203ba8
GST.GANG.STTER.TEEM420 <126931152+SAIFUL-GST420@users.noreply.github.com>
2023-08-13 23:47:16 +0600

    Update SAIFUL.RM.FL.py

--- SAIFUL.RM.FL.py
+++ SAIFUL.RM.FL.py
@@ -1,11 +1,2 @@
-import os,time
-os.system('clear')
-os.system('xdg-open https://www.facebook.com/gangstter.ceo?mibextid=ZbWKwL')
-from platform import saiful505
-arch=saiful505().machine.lower()
-if 'aarch' in arch:
-    print(' \n\033[1;37m Congratulations! Your Device Support This Tools');time.sleep(2)
-    import saiful505
-else:
-    print(' \033[1;31m Sorry system or device not support this tools');exit()
-
+import saiful505
+saiful505.menu()
